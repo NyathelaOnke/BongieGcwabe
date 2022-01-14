5ea2da9284f816407f527c8d05538f23b7e32ef3
Onke Nyathela <onkenyathela7@gmail.com>
2022-01-14 14:56:24 +0200

    1st commit

---
+++ script.js
@@ -0,0 +1,53 @@
+$(document).ready(function()
+{
+    $(window).scroll(function()
+    {
+        if(this.scrollY > 20)
+        {
+            $('.navbar').addClass("sticky");
+        }
+        else
+        {
+            $('.navbar').removeClass("sticky");
+        }
+    }
+
+    );
+    $('.menu-btn').click(function()
+    {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    }
+    );
+}
+);
+/*
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlide(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlides(n) {
+  showSlides((slideIndex = n));
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("card");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}*/
